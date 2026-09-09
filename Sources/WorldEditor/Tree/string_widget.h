2da5f72655d93e0be3eb2a7d56dda8d2569d2bed
SeriousAlexej <seriousalexej@gmail.com>
2026-09-09 11:12:29 +0300

    Add GPLv2 headers

--- Sources/WorldEditor/Tree/string_widget.h
+++ Sources/WorldEditor/Tree/string_widget.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2026 SeriousAlexej (Oleksii Sierov).
+/* Copyright (c) 2026 SeriousAlexej.
 This program is free software; you can redistribute it and/or modify
 it under the terms of version 2 of the GNU General Public License as published by
 the Free Software Foundation
@@ -12,6 +12,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along
 with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+
 #ifndef STRING_WIDGET_AND_DIALOG_H
 #define STRING_WIDGET_AND_DIALOG_H
 
